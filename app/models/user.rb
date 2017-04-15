d5e68bb408d1dfcaf30ec7f8943b15b78f823eae
Skobeltsyn <konstantin@skobeltsyn.com>
2017-04-15 22:41:14 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
             validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                                      uniqueness: { case_sensitive: false }
                                      has_secure_password
-                                     validates :password, length: { minimum: 6 }
+             validates :password_digest, length: { minimum: 6 }
 end
+
